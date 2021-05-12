6a99e5ecafaa7ba8957979e58ee028867144856a
Nathan Conley <nconley2016@gmail.com>
2021-05-12 17:08:27 -0500

    Added timestamps to prints and ability to send multiple messages.

--- client.py
+++ client.py
@@ -1,18 +1,38 @@
 import socket
-import random
+import time
+# import random
+
+
+def print_time(newline):
+	current_time = time.localtime()
+	if newline:
+		print(f"\n[{time.strftime('%H:%M:%S', current_time)}]", end=" ")
+	else:
+		print(f"[{time.strftime('%H:%M:%S', current_time)}]", end=" ")
+
+
+quitFlag = False
 
 # creating basic socket connection to server
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print("Client starting up...\n")
+print_time(0)
+print("Client starting up...")
 
-try:
-	print("Please type the message you would like to send below:\n")
+while not quitFlag:
+	print_time(1)
+	print("Please type the message you would like to send below, or q to quit:\n")
 	message = input()
 
 	if len(message) >= 65:
+		print_time(0)
 		print(f"Message is {len(message)} characters long! Please send a message 64 characters or less in length.\n")
+	elif message == "q":
+		# need to send quit message to the server before powering down
+		client.sendto(str.encode(message), ('127.0.0.1', 1337))
+		print_time(1)
+		print("Quit message received, powering down!\n")
+		quitFlag = True
 	else:
 		client.sendto(str.encode(message), ('127.0.0.1', 1337))
-finally:
-	# cleanup
-	client.close()
+# cleanup
+client.close()
--- server.py
+++ server.py
@@ -1,5 +1,17 @@
 import socket
+import time
 # import threading
+
+
+def print_time(newline):
+    current_time = time.localtime()
+    if newline:
+        print(f"\n[{time.strftime('%H:%M:%S', current_time)}]", end=" ")
+    else:
+        print(f"[{time.strftime('%H:%M:%S', current_time)}]", end=" ")
+
+
+quitFlag = False
 
 PORT = 1337
 IP = "127.0.0.1"
@@ -8,14 +20,24 @@ ADDR = (IP, PORT)
 
 # create server socket and bind it to the pre-defined IP and port
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print_time(0)
 print("Server starting up...\n")
 server.bind(ADDR)
 
 # basic infinite loop to receive data from the sender
-try:
+while not quitFlag:
+    print_time(0)
     print("Waiting to receive message.\n")
     data, addr = server.recvfrom(64)
-    print(f"Received message of length {len(data)}\n")
-    print(f"Message is: {str(data.decode())}")
-finally:
-    server.close()
+    # message needs to be decoded from bytes back to a string
+    message = str(data.decode())
+    if message == "q":
+        print_time(0)
+        print("Quit message received, powering down!\n")
+        quitFlag = True
+    else:
+        print_time(0)
+        print(f"Received message of length {len(message)}\n")
+        print_time(0)
+        print(f"Message is: {message}\n")
+server.close()
